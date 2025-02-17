666bd276247ea17fefaf4a70716a0bdb9b31c4b3
ymwk <yoummwk@gmail.com>
2025-02-17 18:28:00 +0900

    add dist

--- dist/assets/CheckboxComponent-Bkr1isPV.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CaKbATnP.js";const h=a=>{const{type:c="type1",checked:n,name:r,label:s,disabled:t,required:l,onChange:o}=a;return e.jsx(e.Fragment,{children:e.jsxs("label",{className:`checkbox-root ${c}`,children:[e.jsx("input",{type:"checkbox",name:r,checked:n,disabled:t,required:l,onChange:o}),s&&e.jsx("span",{className:"label",children:s})]})})};export{h as C};
