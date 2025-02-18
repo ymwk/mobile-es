b5309f1b312bcb16242b0da952b2ce72167644ef
ymwk <yoummwk@gmail.com>
2025-02-18 10:09:59 +0900

    Updates

--- assets/NotifyPage-BgIM2gdG.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CielpogN.js";import{H as a}from"./HeaderComponent-xaw8gfKb.js";import"./img_pass_pin-Q-80IzAK.js";import"./IconButtonComponent-Q5gGG7rS.js";const m=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(a,{title:"알림"}),r.jsx("main",{className:"portal-main"})]})});export{m as default};
