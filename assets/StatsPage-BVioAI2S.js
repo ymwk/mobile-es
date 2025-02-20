5ebf4f3175d9946f70c7b0db131a3cf78b18baf9
ymwk <yoummwk@gmail.com>
2025-02-20 15:41:52 +0900

    Updates

--- assets/StatsPage-BVioAI2S.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Dj7l0eY2.js";import{H as a}from"./HeaderComponent-DYGvOyX_.js";import"./img_pass_pin-By5_xdyl.js";import"./IconButtonComponent-C2s1SYqT.js";const i=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(a,{title:"통계"}),r.jsx("main",{className:"portal-main"})]})});export{i as default};
