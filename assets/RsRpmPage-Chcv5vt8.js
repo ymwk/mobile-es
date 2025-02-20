7a898c18588ecdcc8f97cf5b9b96bd46f00d8396
ymwk <yoummwk@gmail.com>
2025-02-20 13:51:46 +0900

    Updates

--- assets/RsRpmPage-Chcv5vt8.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-CIrZASbG.js";import{H as r}from"./HeaderComponent-BjHbUxLh.js";import"./img_pass_pin-AE7R2-Rr.js";import"./IconButtonComponent-BaJHF1iP.js";const i=()=>a.jsx(a.Fragment,{children:a.jsxs("div",{className:"wrapper",children:[a.jsx(r,{title:"Runup Pad Management"}),a.jsx("main",{className:"portal-main"})]})});export{i as default};
