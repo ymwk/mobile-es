3352d42aade3dd5bbe12fbbb3193a8fb41e6b9bc
ymwk <yoummwk@gmail.com>
2025-02-26 13:21:59 +0900

    add dist

--- dist/assets/RsSfPage-CTpK5zv2.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-C9TXHPe-.js";import{H as a}from"./HeaderComponent-Bchffbv7.js";import"./img_pass_pin-DyNCfenv.js";import"./IconButtonComponent-DU1eD96o.js";const m=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(a,{title:"Special Flight 신청관리"}),r.jsx("main",{className:"portal-main"})]})});export{m as default};
---
+++ dist/assets/TermsPage-CfxPNsZd.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BOaglB9m.js";import{H as s}from"./HeaderComponent-BG7ZmFHv.js";import"./img_pass_pin-fXel01ns.js";import"./IconButtonComponent-DD6HgbHe.js";const i=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(s,{title:"이용약관"}),r.jsx("main",{className:"portal-main"})]})});export{i as default};
