54a0197cda6fc3117ab2040a69564b3ee6a601ba
ymwk <yoummwk@gmail.com>
2025-02-27 17:01:41 +0900

    Updates

--- assets/RsSfPage-CIeltOAx.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BwDgEk8t.js";import{H as a}from"./HeaderComponent-B36z81EG.js";import"./img_pass_pin-C-g5Rh_s.js";import"./IconButtonComponent-ByAfMyV3.js";const m=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(a,{title:"Special Flight 신청관리"}),r.jsx("main",{className:"portal-main"})]})});export{m as default};
