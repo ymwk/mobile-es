664e76f7e267e81f898bad8e2f0a852f6f4da1e8
ymwk <yoummwk@gmail.com>
2025-02-28 17:42:41 +0900

    Updates

---
+++ assets/RsHpmPage-Dh50xCPy.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-CxxZn04K.js";import{H as r}from"./HeaderComponent-YpJ1tNZu.js";import"./img_pass_pin-UQwf4nAb.js";import"./IconButtonComponent-DDcXdNSH.js";const i=()=>a.jsx(a.Fragment,{children:a.jsxs("div",{className:"wrapper",children:[a.jsx(r,{title:"Hangar Pad Management"}),a.jsx("main",{className:"portal-main"})]})});export{i as default};
--- assets/StatsPage-XVE1vWLf.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ynQsbmYi.js";import{H as a}from"./HeaderComponent-juTfc7XX.js";import"./img_pass_pin-7NzHF4ga.js";import"./IconButtonComponent-BkjAvV3a.js";const i=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(a,{title:"통계"}),r.jsx("main",{className:"portal-main"})]})});export{i as default};
