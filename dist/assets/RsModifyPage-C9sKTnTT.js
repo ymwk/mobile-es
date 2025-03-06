2be00d3b4ca57dca610d5d249e485f349b7fd478
ymwk <yoummwk@gmail.com>
2025-03-06 14:42:41 +0900

    add dist

--- dist/assets/RsModifyPage-C9sKTnTT.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BDXjNJ7p.js";import{H as s}from"./HeaderComponent-BHQvpqgR.js";import"./img_pass_pin-DZhGez2U.js";import"./IconButtonComponent-Dq6BxzRl.js";const m=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(s,{title:"현행배정 변경/삭제 신청"}),r.jsx("main",{className:"portal-main"})]})});export{m as default};
