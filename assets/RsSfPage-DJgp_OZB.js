b56082e691275a934bb19217a99b33b8f49a28e9
ymwk <yoummwk@gmail.com>
2025-03-06 14:42:15 +0900

    Updates

--- assets/RsSfPage-DJgp_OZB.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BDXjNJ7p.js";import{H as a}from"./HeaderComponent-BHQvpqgR.js";import"./img_pass_pin-DZhGez2U.js";import"./IconButtonComponent-Dq6BxzRl.js";const m=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(a,{title:"Special Flight 신청관리"}),r.jsx("main",{className:"portal-main"})]})});export{m as default};
