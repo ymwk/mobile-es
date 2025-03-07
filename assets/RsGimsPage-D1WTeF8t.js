0c0075620cb5480b2156f27e95805cf688c5503c
ymwk <yoummwk@gmail.com>
2025-03-07 14:18:11 +0900

    Updates

---
+++ assets/RsGimsPage-D1WTeF8t.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-4NJodCap.js";import{H as e}from"./HeaderComponent-CjQnNOUG.js";import"./img_pass_pin-DLsEmVwv.js";import"./IconButtonComponent-DzvBoamf.js";const m=()=>s.jsx(s.Fragment,{children:s.jsxs("div",{className:"wrapper",children:[s.jsx(e,{title:"Gate IDD Message Sending"}),s.jsx("main",{className:"portal-main"})]})});export{m as default};
--- assets/RsModifyPage-DCogqwJ6.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DWR_RJCG.js";import{H as s}from"./HeaderComponent-C0YrUNyB.js";import"./img_pass_pin-DsUSS5l9.js";import"./IconButtonComponent-33nTl8w0.js";const m=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(s,{title:"현행배정 변경/삭제 신청"}),r.jsx("main",{className:"portal-main"})]})});export{m as default};
