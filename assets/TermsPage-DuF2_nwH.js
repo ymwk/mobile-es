159f5b67de2f3b33cc8d94a7f1b2b174242c7801
ymwk <yoummwk@gmail.com>
2025-03-12 10:52:53 +0900

    Updates

---
+++ assets/TermsPage-DuF2_nwH.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CygorJVP.js";import{H as s}from"./HeaderComponent-KgMQGfLy.js";import"./img_pass_pin-CJ3yjVMo.js";import"./IconButtonComponent-Bb2g7JdG.js";const i=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(s,{title:"이용약관"}),r.jsx("main",{className:"portal-main"})]})});export{i as default};
