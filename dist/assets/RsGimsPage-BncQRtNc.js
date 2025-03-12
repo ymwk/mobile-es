5aee783f51890701f2bcaf20b1868d9186bb6da4
ymwk <yoummwk@gmail.com>
2025-03-12 10:53:05 +0900

    add dist

---
+++ dist/assets/RsGimsPage-BncQRtNc.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CygorJVP.js";import{H as e}from"./HeaderComponent-KgMQGfLy.js";import"./img_pass_pin-CJ3yjVMo.js";import"./IconButtonComponent-Bb2g7JdG.js";const m=()=>s.jsx(s.Fragment,{children:s.jsxs("div",{className:"wrapper",children:[s.jsx(e,{title:"Gate IDD Message Sending"}),s.jsx("main",{className:"portal-main"})]})});export{m as default};
--- dist/assets/TermsPage-BcHo5JA3.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CiJyN_gL.js";import{H as s}from"./HeaderComponent-D1ol0wRG.js";import"./img_pass_pin-DrXz9NbY.js";import"./IconButtonComponent-D__q9I26.js";const i=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"wrapper",children:[r.jsx(s,{title:"이용약관"}),r.jsx("main",{className:"portal-main"})]})});export{i as default};
